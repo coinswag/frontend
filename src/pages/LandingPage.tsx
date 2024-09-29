87897ce36ef6d992a08abef920377d1b945162fb
Anioke sebastian <aniokechukwudi7@gmail.com>
2024-09-29 22:53:50 +0100

    changed file name

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,8 @@
 import AboutPage from "@/components/landing-page/AboutPage";
-import Faq from "@/components/landing-page/faq/Faq";
+import Faq from "@/components/landing-page/faq/FaqSection";
 import Feature from "@/components/landing-page/feature/Feature";
 import Hero from "@/components/landing-page/Hero";
-import Navbar from "@/components/landing-page/navbar/navbar";
+import NavBar from "@/components/dashboard/navbar/NavBar";
 import TopStories from "@/components/landing-page/top-stores/TopStores";
 import Footer from "@/components/landing-page/footer";
 import { Outlet } from "react-router-dom";
@@ -13,7 +13,7 @@ import Review from "@/components/landing-page/Review";
 function LandingPage() {
 	return (
 		<div className=''>
-			<Navbar />
+			<NavBar />
 			<Hero />
 			<AboutPage />
 			<Feature />
